08e3ec742ac6335b255e7f0d0b8e000eb2f9141f
nuts <nutsinutsy@gmail.com>
2014-01-20 19:17:42 +0100

    basic commit

---
+++ js/client.js
@@ -0,0 +1,38 @@
+var mpd = require('mpd')
+    , parse = require('./parse.js');
+    ;
+
+var client = module.exports = new mpd.connect({
+    port: 6600,
+    host: 'localhost',
+});
+
+
+client.command = function status(str, fn, pfn) {
+    client.sendCommand(str, function(err, str) {
+        fn(err, pfn(str));
+    });
+};
+
+client.next = function next(fn) {
+    client.sendCommand('next', fn);
+};
+
+client.prev= function next(fn) {
+    client.sendCommand('previous', fn);
+};
+
+client.pause = function pause(p, fn) {
+    client.sendCommand(client.cmd('pause', [p]), fn);
+};
+
+client.playid = function playid(p, fn) {
+    client.sendCommand(client.cmd('playid', [p]), fn);
+};
+
+client.collection = function collection(req, fn) {
+    var art = 'Artist';
+    var alb = 'album';
+    var t = 'Evoken';
+    client.command(client.cmd('listallinfo', ['']), fn, parse.playlist);
+};
---
+++ js/parse.js
@@ -0,0 +1,72 @@
+exports.hash = function hash(str) {                              
+    var data = {}
+    , parsed;
+
+    if(str) {
+        str.split('\n').forEach(function(line) {
+            parsed = line.split(':');
+            if(parsed) {
+                data[parsed[0]] = parsed[1];
+            }
+        });
+    }
+    return data;
+};  
+
+function parseFileName(str) {
+    return str.split('/').reverse()[0];
+};
+
+exports.playlist = function playlist(str) {
+    console.log(str);
+    var parsed
+        , data = [];
+    var hasArtist = false,
+        hasAlbum = false,
+        hasGenre = false,
+        hasTitle = false;
+    var first = true;
+    if(str) {
+        str.split('\n').forEach(function(line) {
+            parsed = line.split(':');
+            // Do not process `directory` lines
+            if(parsed && parsed[0] !== 'directory') {
+                if(parsed[0] === 'file') {
+                    if (first == false) {
+                    if (hasTitle == false) {
+                        data[data.length -1]['Title'] = parseFileName(data[data.length-1]['file']);
+                    }
+                    if (hasGenre == false) data[data.length -1]['Genre'] = 
+                                               'Unknow'; 
+                    if (hasArtist== false) data[data.length -1]['Artist'] = 
+                                               'Unknow Artist'; 
+                    if (hasAlbum== false) data[data.length -1]['Album'] = 
+                                               'Unknow Album'; 
+                    }
+                    hasArtist = false, hasGenre = false, hasAlbum = false, hasTitle = false;
+                    first = false;
+                    data.push({});
+                    data[data.length - 1]['Time'] = '00:00';
+                }
+                if (parsed[0] == 'Title') hasTitle = true;
+                if (parsed[0] == 'Genre') hasGenre= true;
+                if (parsed[0] == 'Album') hasAlbum= true;
+                if (parsed[0] == 'Artist') hasArtist= true;
+                console.log(parsed);
+                if (parsed[0] != 'Last-Modified')
+                data[data.length - 1][parsed[0]] = parsed[1];
+            }
+        });
+    }
+    if (first == false) {
+        if (hasTitle == false) data[data.length -1]['Title'] = 
+            data[data.length-1]['Name'];
+        if (hasGenre == false) data[data.length -1]['Genre'] = 
+            'Unknow'; 
+        if (hasArtist== false) data[data.length -1]['Artist'] = 
+            'Unknow Artist'; 
+        if (hasAlbum== false) data[data.length -1]['Album'] = 
+            'Unknow Album'; 
+    }
+    return data;
+};
